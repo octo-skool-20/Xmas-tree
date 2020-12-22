d30be5efaafb9d92ebe9f3acb3aab45f324dee9e
Baptiste Chezaubernard <b.chezaubernard@octo.com>
2020-12-22 19:48:26 +0100

    Init react project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Card from './card/Card.js'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+
+      <body>
+       <Card/>
+      </body>
+    </div>
+  );
+}
+
+export default App;
