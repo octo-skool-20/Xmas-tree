16dfaebf5e5e383c5b092a99196fa14e1d61e49a
Suxue LI <suli@octo.com>
2020-12-22 22:45:39 +0100

    add christmas card

--- src/card/Card.js
+++ src/card/Card.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import Tree from "./Tree";
 
 
 function Card() {
@@ -31,22 +31,13 @@ function Card() {
     return (
         <div id="card">
             <div id="card-inside">
-                <div class="wrap">
-                    <p>Hey son,</p>
-                    <p>I lost the message inside the card 2 times.. and i realsied i am slow at this :'( </p>
-                    <p>Listen kid, I want you to be the happiest today.. and i suck at making people happy..This is probably the only gift i can give you, my word..</p>
-                    <p>I will be there with you in your heart and soul whenever you seek me.
-                    I love you.
-        </p>
-                    <p>always.</p>
-                    <p><a href="http://pandey47.0fees.us/index4.html">NEXT</a></p>
-                    <p class="signed">ZEF</p>
-
+                <div className="wrap">
+                    <Tree/>
                 </div>
             </div>
 
             <div id="card-front">
-                <div class="wrap">
+                <div className="wrap">
                     <h1>Joyeux Noel</h1>
                 </div>
                 <button onClick={open_card} id="open">&gt;</button>
