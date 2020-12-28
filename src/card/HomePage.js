748efc5ee4be3761de3426587f4c60c38740c1c2
Baptiste Chezaubernard <b.chezaubernard@octo.com>
2020-12-28 13:46:13 +0100

    fix: css & layout

--- src/card/HomePage.js
+++ src/card/HomePage.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function HomePage() {
     return (
         <div className="cardContent">
-            <h1>Joyeux Noel</h1>
+            <h1>Joyeux Noël</h1>
             <p>En ces temps de <span className="strong-word">fête</span>, on a tous une mission en <span className="strong-word">tête.</span></p>
 
             <p>Toute la skool 20 vous souhaite un joyeux <span className="strong-word">Noël</span> et une bonne <span className="strong-word">année !</span> </p>
