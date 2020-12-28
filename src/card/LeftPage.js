5a1c2e807f24a18c9a5fd8a0721f5b610f5ceb42
Baptiste Chezaubernard <b.chezaubernard@octo.com>
2020-12-28 13:46:13 +0100

    feat: add left page content

--- src/card/LeftPage.js
+++ src/card/LeftPage.js
@@ -16,12 +16,19 @@ export default function LeftPage() {
 
         }}>
             <div  >
-                <span >
-                    <div className="title">Hello world</div>
-                    <p>BERN, MASI, LELA, CEME, WAJI, MIWE, JUFE, BOXI, JAMU, LRIVA, GASUL, KRAM, CHAZ</p>
-
-                    Bli bla blou bla
-                </span>
+                <div className="card-inside">
+                    <div className="title">Conte de Noël</div>
+                    <p>Nous allons vous conter la véritable histoire des 13 lutins de la skool 20. <br/>
+                    Tous naquirent le 16 novembre 2020, confinés dans leur petite maison. <br/>
+                    Heureusement, Mickael, leur étoile, leur a montré le chemin. <br/>
+                    Tout au long de leur route, ils rencontrèrent différents anges, les formant sur les fondamentaux de la vie (l’agilité, le TDD, l’UX ...), et pour cela ils les remercient. <br/>
+                    Pour découvrir les 13 lutins, passez votre souris sur leur boule, et cliquez dessus ! <br/>
+                    </p>
+                    <p>Happy hanukkah matata!</p>
+                    <hr className="solid-black"/>
+                    
+                    <p>BERN, MASI, LELA, CEME, WAJI, MIWE, JUFE, BOXI, JAMU, ELRI, VAGA, <strong>SULI</strong>, KRAM, CHAZ</p>
+                </div>
             </div>
         </div>
     );
