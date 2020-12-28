20e15de7452bf5d4aa8f4d229568a805157684dd
Baptiste Chezaubernard <b.chezaubernard@octo.com>
2020-12-28 13:46:13 +0100

    feat: change layout just a bit

--- src/card/LeftPage.js
+++ src/card/LeftPage.js
@@ -1,9 +1,25 @@
 import React from "react";
+import Background from '../assets/leftpage-background.png';
+
 
 export default function LeftPage() {
+
+
+
     return (
-        <div >
-            <h1>Bli bla blou bla</h1>
+        <div style={{
+            backgroundImage: `url(${Background})`,
+            backgroundSize: 'cover',
+            width: '100%',
+            height: '100%',
+            margin:0
+
+        }}>
+            <div>
+                <span>
+                    Bli bla blou bla
+                </span>
+            </div>
         </div>
     );
 }
