a60b9ab2b7aee32bb41b1de1c50d3929dbef97fe
Suxue LI <suli@octo.com>
2020-12-29 15:36:29 +0100

    fix console error

--- src/card/HomePage.js
+++ src/card/HomePage.js
@@ -4,14 +4,14 @@ export default function HomePage() {
     return (
         <div className="cardContent">
             <h1>Joyeux Noël</h1>
-            <p style={{paddingTop:'3%', marginBottom:0, marginTop:0}}>
-                <hr className="solid"/>
+            <span style={{paddingTop:'3%', marginBottom:0, marginTop:"10%"}}>
+                <hr className="solid" style={{marginTop:'15%'}}/>
                 En ces temps de <span className="strong-word">fêtes</span>, la SKOOL 20 a une mission en <span className="strong-word">tête. </span>
                 Vous souhaiter un <span className="strong-word">joyeux Noël</span> et une <span className="strong-word"> année exceptionnelle ! </span>
                 Pleins de <span className="strong-word">couleurs</span> pour garder la bonne <span className="strong-word">humeur</span>,
                 D’une <span className="strong-word">teinte</span> à une autre, venez voir les <span className="strong-word">skoolers</span>, dans toute leur <span className="strong-word">splendeur ! </span>
                 Il ne faut que cela <span className="strong-word">tarde</span>, ouvrez vite la <span className="strong-word">carte !</span>
-            </p>
+            </span>
             <hr className="solid"/>
             <p style={{textAlign:'end'}}><span className="strong-word">SKOOL 20 ✨</span></p>
         </div>
